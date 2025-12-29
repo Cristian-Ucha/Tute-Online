de93ac5c38a2d617de90b8be891b59e3e96901c3
Cristian-Ucha <nafmasmenos@gmail.com>
2025-12-29 16:33:12 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
-function saludar() {
-    alert("¡El juego del Tute arrancará aquí!");
+function repartir() {
+    const mesa = document.getElementById("mesa");
+    mesa.innerHTML = "";
+
+    const cartas = [
+        "As de Oros",
+        "Tres de Copas",
+        "Rey de Espadas",
+        "Sota de Bastos",
+        "Siete de Oros"
+    ];
+
+    cartas.forEach(carta => {
+        const div = document.createElement("div");
+        div.textContent = carta;
+        div.className = "carta";
+        mesa.appendChild(div);
+    });
 }
