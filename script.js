91a233850492fb0c1a6284611b5ce010bef4b23e
Cristian-Ucha <nafmasmenos@gmail.com>
2025-12-30 13:47:58 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,16 @@
-// Tamaño EXACTO de cada carta en el sprite
+alert("script.js cargado");
 const ANCHO_CARTA = 208; // 2496 / 12
 const ALTO_CARTA = 319;  // 1595 / 5
 
 function repartir() {
     const mesa = document.getElementById("mesa");
     mesa.innerHTML = "";
-
-    // Cartas de prueba para comprobar alineación
     const cartas = [
-        { fila: 0, columna: 0 },   // primera carta
-        { fila: 0, columna: 11 },  // última de la primera fila
-        { fila: 1, columna: 0 },   // primera de segunda fila
-        { fila: 2, columna: 0 },   // primera de tercera fila
-        { fila: 4, columna: 0 }    // primera de última fila
+        { fila: 0, columna: 0 }, 
+        { fila: 0, columna: 11 }, 
+        { fila: 1, columna: 0 },   
+        { fila: 2, columna: 0 },   
+        { fila: 4, columna: 0 }    
     ];
 
     cartas.forEach(carta => {
