b4ad63f9963f010564d8ab61dc1643206322a6dc
Cristian-Ucha <nafmasmenos@gmail.com>
2025-12-30 14:11:43 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 function repartir() {
     const mesa = document.getElementById("mesa");
+    mesa.innerHTML = "";
 
-    // PRUEBA VISUAL EXTREMA
-    mesa.innerHTML = "<h2>ESTO DEBERÍA VERSE</h2>";
+    const div = document.createElement("div");
+    div.style.width = "208px";
+    div.style.height = "319px";
+    div.style.backgroundColor = "blue";
+
+    mesa.appendChild(div);
 }
