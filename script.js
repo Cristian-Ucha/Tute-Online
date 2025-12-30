a15f11fbe5d26749d0b16b837f6bdde2910e51f8
Cristian-Ucha <nafmasmenos@gmail.com>
2025-12-30 15:27:46 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,16 +3,10 @@ function repartir() {
     mesa.innerHTML = "";
 
     const carta = document.createElement("div");
-
-    carta.style.width = "208px";
-    carta.style.height = "319px";
+    carta.className = "carta";
 
     carta.style.backgroundImage = "url('cartas/baraja.png')";
-    carta.style.backgroundRepeat = "no-repeat";
     carta.style.backgroundPosition = "0px 0px";
-
-    carta.style.border = "3px solid red";
 
     mesa.appendChild(carta);
 }
-
