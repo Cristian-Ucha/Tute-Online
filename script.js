9d1c35fb33679aa7ea53d4d11aa5f14e76dba758
Cristian-Ucha <nafmasmenos@gmail.com>
2025-12-30 13:57:26 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-alert("script.js cargado")
+const ANCHO_CARTA = 208;
+const ALTO_CARTA = 319;
+
+function repartir() {
+    const mesa = document.getElementById("mesa");
+    mesa.innerHTML = "";
+
+    const carta = document.createElement("div");
+    carta.className = "carta";
+
+    carta.style.backgroundPosition = "0px 0px";
+
+    mesa.appendChild(carta);
+}
