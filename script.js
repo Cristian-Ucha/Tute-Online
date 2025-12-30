d535f57c99d94cc9420187c0ff25a45bf9fc8f40
Cristian-Ucha <nafmasmenos@gmail.com>
2025-12-30 14:08:23 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,6 @@
-const ANCHO_CARTA = 208;
-const ALTO_CARTA = 319;
-
 function repartir() {
     const mesa = document.getElementById("mesa");
-    mesa.innerHTML = "";
 
-    const carta = document.createElement("div");
-    carta.className = "carta";
-
-    carta.style.backgroundPosition = "0px 0px";
-
-    mesa.appendChild(carta);
+    // PRUEBA VISUAL EXTREMA
+    mesa.innerHTML = "<h2>ESTO DEBERÍA VERSE</h2>";
 }
